3737d051a3104e34741a9c0d53be83e80edc8e95
ale <ale91.dallas@gmail.com>
2023-12-06 22:29:50 +0100

    to do list migliorata

--- 2023_12_06_Day32/esercizio/assets/js/script2.js
+++ 2023_12_06_Day32/esercizio/assets/js/script2.js
@@ -4,64 +4,30 @@
 
 // per il timer, ho usato session Storage
 
-let saveBtn = document.querySelector("#save-btn");
-let delBtn = document.querySelector("#delete-btn");
+// USER & TIMER
+
+let saveUserButton = document.querySelector("#save-btn");
+let deleteUserButton = document.querySelector("#delete-btn");
 let svuotaBtn = document.querySelector("#svuota-btn");
 let timerDiv = document.querySelector("#timer span");
-
-console.log(localStorage);
-let counter;
 let lastUserDiv = document.querySelector("div.last-user");
 
+let counter;
+
 window.addEventListener("load", () => {
-  let savedCounter = localStorage.getItem("counter");
-  if (!savedCounter) {
-    counter = 1;
-    localStorage.setItem("counter", counter);
-    // console.log(`if: ${counter}`)
-  } else {
-    counter = savedCounter;
-    // console.log(`else: ${counter}`);
-  }
-
-  let savedTime = sessionStorage.getItem("time");
-  if (!savedTime) {
-    time = 1;
-    sessionStorage.setItem("time", time);
-  } else {
-    time = savedTime;
-  }
-  setInterval(() => {
-    sessionStorage.setItem("time", time);
-    timerDiv.innerText = sessionStorage.getItem("time");
-    time++;
-  }, 1000);
+  initializeUserCounter();
+  initializeTimer();
+  clock();
 });
 
-saveBtn.addEventListener("click", (e) => {
+saveUserButton.addEventListener("click", (e) => {
   e.preventDefault();
-  let user = document.forms[0][0].value;
-
-  if (user) {
-    newUser = {
-      id: counter,
-      username: user,
-    };
-
-    lastUserDiv.innerText = newUser.username;
-
-    localStorage.setItem(`user_${counter} `, JSON.stringify(newUser));
-
-    counter++;
-    localStorage.setItem("counter", `${counter}`);
-  }
+  addUser();
 });
 
-delBtn.addEventListener("click", (e) => {
+deleteUserButton.addEventListener("click", (e) => {
   e.preventDefault();
-
   lastUserDiv.innerText = "";
-  console.log(localStorage.key(`user_${localStorage.length}`));
 });
 
 svuotaBtn.addEventListener("click", (e) => {
@@ -69,78 +35,120 @@ svuotaBtn.addEventListener("click", (e) => {
   localStorage.clear();
 });
 
+function initializeUserCounter() {
+  let savedCounter = localStorage.getItem("counter");
 
+  if (!savedCounter) {
+    counter = 1;
+    localStorage.setItem("counter", counter);
+  } else {
+    counter = savedCounter;
+  }
+}
 
+function initializeTimer() {
+  let savedTime = sessionStorage.getItem("time");
+  if (!savedTime) {
+    time = 1;
+    sessionStorage.setItem("time", time);
+  } else {
+    time = savedTime;
+  }
+}
 
+function clock() {
+  setInterval(() => {
+    sessionStorage.setItem("time", time);
+    timerDiv.innerText = sessionStorage.getItem("time");
+    time++;
+  }, 1000);
+}
 
-// To DO LIST
+function addUser() {
+  let user = document.forms[0][0].value;
+  if (user) {
+    newUser = {
+      id: counter,
+      username: user,
+    };
+
+    lastUserDiv.innerText = newUser.username;
+    localStorage.setItem(`user_${counter} `, JSON.stringify(newUser));
+    counter++;
+    localStorage.setItem("counter", `${counter}`);
+  }
+  document.forms[0][0].value = "";
+}
+
+// TO DO LIST
 // prove di stateful
+
 let taskSpace = document.querySelector("#listaTodo");
 let addTaskBtn = document.querySelector("#todo-btn");
 
 let tasksTot;
 
 window.addEventListener("load", () => {
-  let tasksCounter = localStorage.getItem("tasksTot");
-  if (!tasksCounter) {
-    tasksTot = 0;
-    localStorage.setItem("tasksTot", tasksTot);
-    // console.log(`if: ${tasksTot}`)
-  } else {
-    tasksTot = tasksCounter;
-    // console.log(`else: ${tasksTot}`);
-
-    for (let i = 0; i < localStorage.length; i++) {
-      if (localStorage.key(i).startsWith("task_")) {
-        let oldTask = JSON.parse(localStorage.getItem(localStorage.key(i)));
-        let idFinder = localStorage.key(i).slice(5);
-
-        console.log(idFinder);
-        let li = document.createElement("li");
-
-        if (oldTask.isDone) {
-        }
-        li.innerHTML = `<div class="riga">${oldTask.taskName}</div> <button id="task-id-${idFinder}" class="remove-task btn btn-danger"> X</button>`;
-        taskSpace.appendChild(li);
-        li.children[0].classList.add("done");
-      }
-    }
-  }
-
-  let removeBtns = document.querySelectorAll(".remove-task");
-
-  console.dir(removeBtns);
-  removeBtns.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-      let targetId = e.target.id.slice(8);
-      console.log(targetId);
-      localStorage.removeItem(`task_${targetId}`);
-      e.target.parentNode.remove();
-    });
-  });
-
-  removeBtns = "";
+  initializeTasksCounter();
+  retrieveSavedTasks();
+  addRemoveButtons();
 });
 
 addTaskBtn.addEventListener("click", (e) => {
   e.preventDefault();
+  addNewTask();
+  addRemoveButtons();
+  document.forms[1][0].value = "";
+});
+
+statefulSignAsDone();
+
+function initializeTasksCounter() {
+  let tasksCounter = localStorage.getItem("tasksTot");
+  if (!tasksCounter) {
+    tasksTot = 0;
+    localStorage.setItem("tasksTot", tasksTot);
+  } else {
+    tasksTot = tasksCounter;
+  }
+}
+
+function retrieveSavedTasks() {
+  for (let i = 0; i < localStorage.length; i++) {
+    if (localStorage.key(i).startsWith("task_")) {
+      let oldTask = JSON.parse(localStorage.getItem(localStorage.key(i)));
+      let li = document.createElement("li");
+
+      let idFinder = localStorage.key(i).slice(5);
+      li.innerHTML = `<div class="riga">${oldTask.taskName}</div> <button id="task-id-${idFinder}" class="remove-task btn btn-danger"> X </button>`;
+
+      taskSpace.appendChild(li);
+
+      if (oldTask.isDone && !li.classList.contains("done")) {
+        li.children[0].classList.add("done");
+      }
+    }
+  }
+}
+
+function addNewTask() {
   let newTask = document.forms[1][0].value;
+  tasksTot++;
+  localStorage.setItem("tasksTot", `${tasksTot}`);
   let li = document.createElement("li");
   li.innerHTML = `<div class="riga">${newTask}</div> <button id="task-id-${tasksTot}" class="remove-task btn btn-danger"> X </i></button>`;
   taskSpace.appendChild(li);
-
-  tasksTot++;
-  localStorage.setItem("tasksTot", `${tasksTot}`);
 
   let taskToSave = {
     taskName: newTask,
     isDone: false,
   };
   localStorage.setItem(`task_${tasksTot}`, JSON.stringify(taskToSave));
+}
 
+function addRemoveButtons() {
   let removeBtns = document.querySelectorAll(".remove-task");
 
-  console.dir(removeBtns);
   removeBtns.forEach((btn) => {
     btn.addEventListener("click", (e) => {
       let targetId = e.target.id.slice(8);
@@ -149,37 +157,29 @@ addTaskBtn.addEventListener("click", (e) => {
       e.target.parentNode.remove();
     });
   });
+}
 
-  removeBtns = "";
-  document.forms[1][0].value = '';
-});
-
-taskSpace.addEventListener("click", (e) => {
+function statefulSignAsDone() {
+  taskSpace.addEventListener("click", (e) => {
     e.stopImmediatePropagation();
-    // let allLi = taskSpace.querySelectorAll('li');
-    // allLi.forEach((li) => {
-    //     li.classList.remove('done');
-    // })
-  if (e.target.classList.contains("riga")) {
-    let idTask = e.target.nextElementSibling.id.slice(8);
-    console.log(idTask)
 
-    if (!e.target.classList.contains("done")) {
-      e.target.classList.add("done");
-       let fetchTask = JSON.parse(localStorage.getItem(`task_${idTask}`));
-       fetchTask.isDone = true;
-       console.log(fetchTask);
+    if (e.target.classList.contains("riga")) {
+      let idTask = e.target.nextElementSibling.id.slice(8);
+      console.log(idTask);
+
+      if (!e.target.classList.contains("done")) {
+        e.target.classList.add("done");
+        let fetchTask = JSON.parse(localStorage.getItem(`task_${idTask}`));
+        fetchTask.isDone = true;
+        console.log(fetchTask);
         localStorage.setItem(`task_${idTask}`, JSON.stringify(fetchTask));
-
-    }
-    else{
+      } else {
         e.target.classList.remove("done");
         let fetchTask = JSON.parse(localStorage.getItem(`task_${idTask}`));
         fetchTask.isDone = false;
         console.log(fetchTask);
         localStorage.setItem(`task_${idTask}`, JSON.stringify(fetchTask));
+      }
     }
-  }
-
- 
-});
+  });
+}
