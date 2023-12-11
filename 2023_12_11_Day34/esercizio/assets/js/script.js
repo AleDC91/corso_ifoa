a49d27d0c4157335860e68ac977ab390d523dcf4
ale <ale91.dallas@gmail.com>
2023-12-11 22:26:39 +0100

    pagination

--- 2023_12_11_Day34/esercizio/assets/js/script.js
+++ 2023_12_11_Day34/esercizio/assets/js/script.js
@@ -8,18 +8,21 @@ const loadSecondaryImagesButton = document.querySelector(
 const primaryQuery = "dolomites";
 const secondaryQuery = "beer";
 const album = document.querySelector(".photos");
-
+const pagination = document.querySelector("ul.pagination");
+let perPage = 40;
 let currentURL = window.location.href;
 const main = document.querySelector("main");
-
+let query = "";
 window.addEventListener("load", () => {
   if (currentURL.includes("index.html")) {
     loadImagesButton.addEventListener("click", () => {
-      loadPhotos(primaryQuery);
+      query = primaryQuery;
+      loadPhotos(query, 1, perPage);
     });
 
     loadSecondaryImagesButton.addEventListener("click", () => {
-      loadPhotos(secondaryQuery);
+      query = secondaryQuery;
+      loadPhotos(query, 1, perPage);
     });
 
     // hide button
@@ -27,62 +30,28 @@ window.addEventListener("load", () => {
       console.log(e.target);
       if (e.target.classList.contains("hide-btn")) {
         e.target.parentNode.parentNode.parentNode.parentNode.parentNode.remove();
-    // view button
+        // view button
       } else if (e.target.classList.contains("view-btn")) {
         let id = e.target.dataset.bsTarget;
-        fillModal(id)
+        fillModal(id);
       }
     });
 
     // search
     document.forms[0].querySelector("button").addEventListener("click", () => {
-      loadPhotos(document.forms[0][0].value);
+      query = document.forms[0][0].value;
+      loadPhotos(query, 1, perPage);
+
     });
   } else if (currentURL.includes("photoInfo.html")) {
     fetchPhoto();
   }
 });
 
-function loadPhotos(query) {
+function loadPhotos(query, page, perPage) {
   album.innerHTML = "";
-  const URI = `https://api.pexels.com/v1/search?query=${query}/`;
-  fetch(URI, {
-    method: "GET",
-    headers: {
-      Authorization: PEXELS_API_KEY,
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      const photos = data.photos;
-      console.log(photos);
-      photos.forEach((photo) => {
-        const card = document.createElement("div");
-        card.classList.add("col-md-4", "col-sm-6");
-        card.innerHTML = `
-          <div class="card mb-4 shadow-sm">
-          <a href="photoInfo.html?id=${photo.id}&title=${photo.alt}&author=${photo.photographer}&authorSite=${photo.photographer_url}&avgColor=${photo.avg_color}">
-              <img src="${photo.src.original}" alt="${photo.alt}" class="img-fluid" />
-          </a>
-              <div class="card-body">
-                  <h5 class="card-title">${photo.alt}</h5>
-                  <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                          <button type="button" class="view-btn btn btn-sm btn-outline-secondary" data-bs-toggle="modal" data-bs-target="${photo.id}">
-                              View
-                          </button>
-                          <button type="button" class="hide-btn btn btn-sm btn-outline-secondary">
-                              Hide
-                          </button>
-                      </div>
-                      <small class="text-muted">${photo.id}</small>
-                  </div>
-              </div>
-          </div>
-          `;
-        album.appendChild(card);
-      });
-    });
+  const URI = `https://api.pexels.com/v1/search?query=${query}&page=${page}&per_page=${perPage}`;
+  renderPhotos(URI)
 }
 
 function fetchPhoto() {
@@ -127,13 +96,87 @@ function fillModal(id) {
   })
     .then((response) => response.json())
     .then((photo) => {
-        let modal = document.querySelector(".modal");
-        modal.querySelector('img').src = photo.src.original;
-        modal.querySelector('img').alt = photo.title;
-        modal.id = photo.id;
-        modal.querySelector('h1').innerText = photo.alt;
-        modal.querySelector('h5').innerText = photo.photographer;
-        const modalInstance = new bootstrap.Modal(modal);
-        modalInstance.show();
+      let modal = document.querySelector(".modal");
+      modal.querySelector("img").src = photo.src.original;
+      modal.querySelector("img").alt = photo.title;
+      modal.id = photo.id;
+      modal.querySelector("h1").innerText = photo.alt;
+      modal.querySelector("h5").innerText = photo.photographer;
+      const modalInstance = new bootstrap.Modal(modal);
+      modalInstance.show();
     });
+}
+
+pagination.addEventListener("click", (e) => {
+
+e.preventDefault();
+album.innerHTML = "";
+renderPhotos(e.target.href)
+// console.log(e.target);
+
+
+})
+
+function renderPhotos(uri) {
+  fetch(uri, {
+    method: "GET",
+    headers: {
+      Authorization: PEXELS_API_KEY,
+    }
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const photos = data.photos;
+      console.log(photos);
+        console.log(data);
+      photos.forEach((photo) => {
+        const card = document.createElement("div");
+        card.classList.add("col-md-4", "col-sm-6");
+        card.innerHTML = `
+          <div class="card mb-4 shadow-sm">
+          <a href="photoInfo.html?id=${photo.id}&title=${photo.alt}&author=${photo.photographer}&authorSite=${photo.photographer_url}&avgColor=${photo.avg_color}">
+              <img src="${photo.src.original}" alt="${photo.alt}" class="img-fluid" />
+          </a>
+              <div class="card-body">
+                  <h5 class="card-title">${photo.alt}</h5>
+                  <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                          <button type="button" class="view-btn btn btn-sm btn-outline-secondary" data-bs-toggle="modal" data-bs-target="${photo.id}">
+                              View
+                          </button>
+                          <button type="button" class="hide-btn btn btn-sm btn-outline-secondary">
+                              Hide
+                          </button>
+                      </div>
+                      <small class="text-muted">${photo.id}</small>
+                  </div>
+              </div>
+          </div>
+          `;
+        album.appendChild(card);
+
+        
+      });
+      pagination.innerHTML = "";
+      let prev = document.createElement("li");
+      prev.classList.add("page-item");
+      prev.innerHTML = `<a class="page-link" href="${data.prev_page}">Previous</a>`;
+      pagination.appendChild(prev);
+      let pages = Math.ceil(data.total_results / data.per_page);
+      // console.log(pages);
+      for (let i = 1; i <= pages; i++) {
+        let pageNumber = document.createElement("li");
+        pageNumber.classList.add("page-item");
+        if (i === data.page){
+          pageNumber.classList.add("active")
+        }
+        pageNumber.innerHTML = `<a class="page-link" href="https://api.pexels.com/v1/search?query=${query}&page=${i}&per_page=${perPage}">${i}</a>`;
+        pagination.appendChild(pageNumber);
+      }
+      let next = document.createElement("li");
+      next.classList.add("page-item");
+      next.innerHTML = `<a class="page-link" href="${data.next_page}">Next</a>`;
+      pagination.appendChild(next);
+
+    })
 }
