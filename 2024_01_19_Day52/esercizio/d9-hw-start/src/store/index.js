f56c20ef19f87e163642b32e79b297d918490755
ale <ale91.dallas@gmail.com>
2024-01-31 11:51:07 +0100

    ultimo frontend

--- 2024_01_19_Day52/esercizio/d9-hw-start/src/store/index.js
+++ 2024_01_19_Day52/esercizio/d9-hw-start/src/store/index.js
@@ -2,8 +2,9 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import favouritesReducer from "../reducers/favouritesReducer";
 import jobsReducer from "../reducers/jobsReducer";
 import { thunk } from "redux-thunk";
-
-
+import storage from "redux-persist/lib/storage"
+import { persistReducer, persistStore } from "redux-persist";
+import { encryptTransform } from 'redux-persist-transform-encrypt';
 
 
 const initialState = {
@@ -20,9 +21,26 @@ const bigReducer = combineReducers({
   jobs: jobsReducer,
 });
 
+
+const persistentConfig = {
+  key: "root",
+  storage,
+  transforms: [
+    encryptTransform({
+      secretKey: 'my-super-secret-key',
+      onError: function (error) {
+        console.log(error)
+      },
+    })]
+}
+
+
+const persistedReducer = persistReducer(persistentConfig, bigReducer)
+
 export const store = createStore(
- bigReducer,
+ persistedReducer,
   initialState,
   applyMiddleware(thunk)
 );
 
+export const persistor = persistStore(store)
